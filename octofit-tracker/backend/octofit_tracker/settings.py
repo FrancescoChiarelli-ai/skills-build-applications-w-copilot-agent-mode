e39d8d6c0ea2f085a69555b7d1153c7c136ee8ca
FrancescoChiarelli-ai <francesco.chiarelli@xitaso.com>
2025-10-24 13:15:20 +0000

    feat:  include our codespace name for the URL endpoint

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-n*aoov4x8671kf_!%&bz2gqlh$3^-n-l86yb&k)k_@-n^l4xo!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME', '')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
